c74e7f5aa36d99bc470fec70dc1d86c995c97821
Andrei Burvel <burvelandrei@gmail.com>
2024-01-28 12:44:13 +0300

    создана_точка_входа_в_бот

---
+++ bot.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import user_handler, other_handler
+from config import Config, load_config
+
+
+async def main():
+    config:Config = load_config()
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher
+
+    dp.include_router(user_handler.rt)
+    dp.include_router(other_handler.rt)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
