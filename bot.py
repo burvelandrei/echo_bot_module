c84af66a3fed727f8700880c7515ff4f2e9ec930
Andrei Burvel <burvelandrei@gmail.com>
2024-01-28 13:35:11 +0300

    исправлены_ошибки

--- bot.py
+++ bot.py
@@ -7,13 +7,13 @@ from config import Config, load_config
 async def main():
     config:Config = load_config()
     bot = Bot(token=config.tg_bot.token)
-    dp = Dispatcher
+    dp = Dispatcher()
 
     dp.include_router(user_handler.rt)
     dp.include_router(other_handler.rt)
 
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling()
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
